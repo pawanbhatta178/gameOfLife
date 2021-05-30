21dfb005dbefee3a10e635e7389b2e081008f0a2
pawan <pawanbhatta178@gmail.com>
2021-05-30 12:20:37 -0400

    React

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import { Grid, CellData } from './Containers/Grid';
+import { Grid, RowData } from './Containers/Grid';
 
 
 
-const Data: CellData[] = [
-  {row:0,col:0,alive:true},
-  {row:0,col:1, alive:false}
+
+let Data: RowData[] = [
+  {
+    id:"0",
+    payload: [
+      { col: 0, row: 0, alive: true },
+      { row: 0, col: 1, alive: false }
+    ]
+  },
+  {
+    id:"1",
+    payload: [
+      { row: 1, col: 0, alive: true },
+      { row: 1, col: 1, alive: false }
+    ]
+  }
 ]
 
 function App() {
   return (
     <div className="">
-      <Grid gridData={Data} numRows={9} numCols={9 }/>
+      <Grid payload={Data} numRows={9} numCols={9 }/>
     </div>
   );
 }
--- src/Containers/Grid/index.tsx
+++ src/Containers/Grid/index.tsx
@@ -8,18 +8,25 @@ export interface CellData{
     
 }
 
+export interface RowData{
+    id: string;
+    payload: CellData[]
+}
+  
+
+
 
 export interface GridProps{
     className?: string;
     numCols: number;
     numRows: number;
-     gridData?: CellData[]
+    payload: RowData[]
 }
 
 
 
 
-const Grid:React.FC<GridProps> = ({className, gridData, numCols, numRows}) => {
+const Grid:React.FC<GridProps> = ({className, payload, numCols, numRows}) => {
     const tableRef = useRef<HTMLTableElement|null>(null);
 
 
@@ -27,16 +34,28 @@ const Grid:React.FC<GridProps> = ({className, gridData, numCols, numRows}) => {
         <table ref={tableRef}>
             <tbody>
                 {
-                    gridData?.map(item => <GridCell row={item.row} col={item.col} alive={ item.alive}/>)
+                    payload?.map((item, index) => <GridRow key={`${item.id}` } {...item}/>)
               }
           </tbody>
         </table>
     )
 }
 
+const GridRow: React.FC<RowData> = ({id,payload}) => {
+    return <tr id={`${id}`} >
+        {payload.map((item, index) => <GridCell key={`${item.row}-${item.col}` } {...item}/>)}
+    </tr>
+}
+
+
+const GridCellStyle = `w-4 h-4 border border-purple-300 `;
+const GridCellAliveStyle = `w-4 h-4 border bg-purple-600 border-purple-300 `;
+
+
+
 
 const GridCell: React.FC<CellData> = ({row, col,alive}) => {
-    return <td  id={row + "-" + col} ></td>
+    return <td onClick={()=>console.log(`clicked ${row}`) } className={alive?GridCellAliveStyle:GridCellStyle } id={row + "-" + col} ></td>
 }
 
 
